6e5d5d29da70352938a6d37f4d0e46482e331ee1
DiveshGaonkar99 <gaonkardivesh14@gmail.com>
2024-12-26 14:53:11 +0530

    changes made

--- e-com.js
+++ e-com.js
@@ -1,66 +1,65 @@
-const express = require("express")
-const jwt = require("jsonwebtoken")
-const JWT_SECRET = "Divesh_||_Aryan";
-const cors = require("cors")
-
-
-const mongoose = require('mongoose');
-const { usersINFOModel, OrdersModel , ProductINFOModel } = require("./e-comDB");
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const { usersINFOModel, OrdersModel, ProductINFOModel } = require("./e-comDB");
 
 const app = express();
-
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
+dotenv.config();
 
 // const mongoString = " mongodb+srv://pachandiaryan:kIYEHHvtlC5s09NT@aryanpachandi.bew7r.mongodb.net/E-commerce"
 // const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://pachandiaryan:kIYEHHvtlC5s09NT@aryanpachandi.bew7r.mongodb.net/E-Commerce');
+mongoose.connect(process.env.MONGO_URL);
 
 // signup and sign-in will be same for seller and consumer
 //  the diff will be role {seller || consumer}
 
-app.post('/sign-up',async (req,res)=>{
-    const name = req.body.name;
-    const emailID = req.body.emailID
-    const password = req.body.password
-    const role = req.body.role
+app.post("/sign-up", async (req, res) => {
+  const name = req.body.name;
+  const emailID = req.body.emailID;
+  const password = req.body.password;
+  const role = req.body.role;
 
-    await usersINFOModel.create({
-        name :name,
-        emailID :emailID,
-        password :password,
-        role : role,
-    })
-     
-    res.json({
-        msg : " u r signed in successfully"
-    })
-})
+  await usersINFOModel.create({
+    name: name,
+    emailID: emailID,
+    password: password,
+    role: role,
+  });
 
-app.post('/sign-in',async (req,res)=>{
-  const emailID = req.body.emailID
-  const password = req.body.password
+  res.json({
+    msg: " u r signed in successfully",
+  });
+});
+
+app.post("/sign-in", async (req, res) => {
+  const emailID = req.body.emailID;
+  const password = req.body.password;
 
   const response = await usersINFOModel.findOne({
-    emailID : emailID,
-    password : password,
-  })
-  if(response){
-    const token = jwt.sign({id:response._id.toString()},JWT_SECRET );
+    emailID: emailID,
+    password: password,
+  });
+  if (response) {
+    const token = jwt.sign(
+      { id: response._id.toString() },
+      process.env.JWT_SECRET
+    );
     res.json({
-        authentication_token : token
-    })
-
-  }else{
+      authentication_token: token,
+    });
+  } else {
     res.send(403).json({
-        msg : "email or password incorrect "
-    })
+      msg: "email or password incorrect ",
+    });
   }
-})
-//sign up signin working 
+});
+//sign up signin working
 
-
-app.listen(3000,()=>{
-    console.log(">>working on localhost:3000")
-})
+app.listen(3000, () => {
+  console.log(">>working on localhost:3000");
+});
